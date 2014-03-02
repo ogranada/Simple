8f4180e8be713b44e3e8fb66753ac6492b9272fe
ogranada <oscar.andres.granada@gmail.com>
2014-03-02 03:00:42 -0500

    POST, PUT and DELETE methods added to RESTDataSource

--- src/com/framework/simple/datalayer/sources/RESTDataSource.java
+++ src/com/framework/simple/datalayer/sources/RESTDataSource.java
@@ -3,18 +3,26 @@ package com.framework.simple.datalayer.sources;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
@@ -25,7 +33,7 @@ import org.json.JSONObject;
 import com.framework.simple.interfaces.Callback;
 import com.framework.simple.interfaces.DataSource;
 
-public class RESTDataSource implements DataSource{
+public class RESTDataSource implements DataSource {
 
 	CookieStore cookieStore;
 	HttpContext localContext;
@@ -36,9 +44,9 @@ public class RESTDataSource implements DataSource{
 	String host;
 	int port;
 	String uri;
-	
+
 	public RESTDataSource(String host, int port) {
-		this.host = host.contains("http://")?host.split("http://")[1]:host;
+		this.host = host.contains("http://") ? host.split("http://")[1] : host;
 		this.port = port;
 		uri = String.format("http://%s:%d/", this.host, this.port);
 		cookieStore = new BasicCookieStore();
@@ -46,83 +54,213 @@ public class RESTDataSource implements DataSource{
 		localContext.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
 		dhttpclient = new DefaultHttpClient();
 	}
-	
+
 	public void setAuthData(String username, String password) {
 		this.username = username;
 		this.password = password;
-		dhttpclient.getCredentialsProvider().setCredentials(new AuthScope(host, AuthScope.ANY_PORT), new UsernamePasswordCredentials(username, password));
-        HttpGet dhttpget = new HttpGet(uri);
-        dhttpget.setHeader("Content-Type", "application/json;charset=UTF-8");
-        System.out.println("executing request " + dhttpget.getRequestLine());
-        try {
-            HttpResponse dresponse = dhttpclient.execute(dhttpget, localContext);
-            StatusLine resp = dresponse.getStatusLine();
-            System.out.println( resp );
+		dhttpclient.getCredentialsProvider().setCredentials(
+				new AuthScope(host, AuthScope.ANY_PORT),
+				new UsernamePasswordCredentials(username, password));
+		HttpGet dhttpget = new HttpGet(uri);
+		dhttpget.setHeader("Content-Type", "application/json;charset=UTF-8");
+		System.out.println("executing request " + dhttpget.getRequestLine());
+		try {
+			HttpResponse dresponse = dhttpclient
+					.execute(dhttpget, localContext);
+			StatusLine resp = dresponse.getStatusLine();
+			System.out.println(resp);
 		} catch (Exception e) {
-			System.out.println( "Error: "+e );
+			System.out.println("Error: " + e);
+		}
+	}
+
+	private String parseParamsForGet(Map<String, String> params) {
+		if (params == null || (params != null && params.size() == 0)) {
+			return "";
+		}
+		List<NameValuePair> _params = new LinkedList<NameValuePair>();
+		for (String key : params.keySet()) {
+			_params.add(new BasicNameValuePair(key, params.get(key)));
+		}
+		return "?" + URLEncodedUtils.format(_params, "UTF-8");
+	}
+
+	@Override
+	public void getData(String apiSection, Map<String, String> params,
+			Callback callback) {
+		try {
+			String spec = apiSection;
+			spec = spec.startsWith("/") ? spec.substring(1) : spec;
+			spec = spec.endsWith("/") ? spec : spec + "/";
+			HttpGet dhttpget = new HttpGet(uri + spec
+					+ parseParamsForGet(params));
+			dhttpget.setHeader("Content-Type", "application/json;charset=UTF-8");
+			try {
+				HttpResponse dresponse = dhttpclient.execute(dhttpget,
+						localContext);
+				if (callback != null) {
+					List<Map<String, Object>> l = new ArrayList<Map<String, Object>>(
+							0);
+					String respStr = EntityUtils
+							.toString(dresponse.getEntity());
+					if (respStr.startsWith("[")) {
+						JSONArray arr = new JSONArray(respStr);
+						for (int i = 0; i < arr.length(); i++) {
+							l.add(toMap(arr.getJSONObject(i)));
+						}
+					} else if (respStr.startsWith("{")) {
+						JSONObject json = new JSONObject(respStr);
+						Map<String, Object> mapa = toMap(json);
+						l.add(mapa);
+					}
+					callback.onFinish(l);
+				}
+			} catch (Exception e) {
+				System.out.println("Error: " + e);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private StringEntity parsePostParams(Map<String, String> params)
+			throws Exception {
+		if (params == null || (params != null && params.size() == 0)) {
+			return new StringEntity("");
+		}
+		JSONObject dato = new JSONObject();
+		for (String key : params.keySet()) {
+			dato.put(key, params.get(key));
+		}
+		return new StringEntity(dato.toString());
+	}
+
+	@Override
+	public void postData(String apiSection, Map<String, String> params,
+			Callback callback) {
+		try {
+			String spec = apiSection;
+			spec = spec.startsWith("/") ? spec.substring(1) : spec;
+			spec = spec.endsWith("/") ? spec : spec + "/";
+			HttpPost dhttppost = new HttpPost(uri + spec);
+			dhttppost.setHeader("Content-Type",
+					"application/json;charset=UTF-8");
+			try {
+				dhttppost.setEntity(parsePostParams(params));
+				HttpResponse dresponse = dhttpclient.execute(dhttppost,
+						localContext);
+				if (callback != null) {
+					List<Map<String, Object>> l = new ArrayList<Map<String, Object>>(
+							0);
+					String respStr = EntityUtils
+							.toString(dresponse.getEntity());
+					if (respStr.startsWith("[")) {
+						JSONArray arr = new JSONArray(respStr);
+						for (int i = 0; i < arr.length(); i++) {
+							l.add(toMap(arr.getJSONObject(i)));
+						}
+					} else if (respStr.startsWith("{")) {
+						JSONObject json = new JSONObject(respStr);
+						Map<String, Object> mapa = toMap(json);
+						l.add(mapa);
+					}
+					callback.onFinish(l);
+				}
+			} catch (Exception e) {
+				System.out.println("Error: " + e);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
 	@Override
-	public void getData(String apiSection, Callback callback) {
-        try
-        {     
-        	String spec = apiSection;
-        	spec = spec.startsWith("/")?spec.substring(1):spec;
-        	spec = spec.endsWith("/")?spec:spec+"/";
-        	HttpGet dhttpget = new HttpGet(uri+spec);
-    		dhttpget.setHeader("Content-Type", "application/json;charset=UTF-8");
-            try {
-                HttpResponse dresponse = dhttpclient.execute(dhttpget, localContext);
-                if(callback!=null){
-                	List<Map<String,Object>> l = new ArrayList<Map<String,Object>>(0);
-                	String respStr = EntityUtils.toString(dresponse.getEntity());
-                	if(respStr.startsWith("[")){
-                		JSONArray arr = new JSONArray(respStr);
-                		for (int i = 0; i < arr.length(); i++) {
+	public void putData(String apiSection, Map<String, String> params,
+			Callback callback) {
+		try {
+			String spec = apiSection;
+			spec = spec.startsWith("/") ? spec.substring(1) : spec;
+			spec = spec.endsWith("/") ? spec : spec + "/";
+			HttpPut dhttpput = new HttpPut(uri + spec);
+			dhttpput.setHeader("Content-Type",
+					"application/json;charset=UTF-8");
+			try {
+				dhttpput.setEntity(parsePostParams(params));
+				HttpResponse dresponse = dhttpclient.execute(dhttpput,
+						localContext);
+				if (callback != null) {
+					List<Map<String, Object>> l = new ArrayList<Map<String, Object>>(
+							0);
+					String respStr = EntityUtils
+							.toString(dresponse.getEntity());
+					if (respStr.startsWith("[")) {
+						JSONArray arr = new JSONArray(respStr);
+						for (int i = 0; i < arr.length(); i++) {
 							l.add(toMap(arr.getJSONObject(i)));
 						}
-                	} else if(respStr.startsWith("{")){
-                		JSONObject json = new JSONObject(respStr);
-                		Map<String, Object> mapa = toMap(json);
-                		l.add(mapa);
-                	}
-                	callback.onFinish(l);
-                }
-    		} catch (Exception e) {
-    			System.out.println( "Error: "+e );
-    		}
-        	/*
-            HttpPost post = new HttpPost("http://192.168.100.7:8000/autores/");
-			post.setHeader("content-type", "application/json; charset=utf-8");
-			JSONObject json = new JSONObject();
-			json.put("nombre", "pedro");
-			json.put("apellido", "paramo");
-			StringEntity entity = new StringEntity(json.toString());
-			post.setEntity(entity);
-			HttpResponse resp2 = dhttpclient.execute(post, localContext);
-            System.out.println( resp2.getStatusLine() );
-            String respStr = EntityUtils.toString(resp2
-					.getEntity());
-			*/
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+					} else if (respStr.startsWith("{")) {
+						JSONObject json = new JSONObject(respStr);
+						Map<String, Object> mapa = toMap(json);
+						l.add(mapa);
+					}
+					callback.onFinish(l);
+				}
+			} catch (Exception e) {
+				System.out.println("Error: " + e);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	@Override
+	public void deleteData(String apiSection, String id,
+			Callback callback) {
+		try {
+			String spec = apiSection;
+			spec = spec.startsWith("/") ? spec.substring(1) : spec;
+			spec = spec.endsWith("/") ? spec : spec + "/";
+			HttpDelete dhttpdel = new HttpDelete(uri + spec + id);
+			dhttpdel.setHeader("Content-Type", "application/json;charset=UTF-8");
+			try {
+				HttpResponse dresponse = dhttpclient.execute(dhttpdel,
+						localContext);
+				if (callback != null) {
+					List<Map<String, Object>> l = new ArrayList<Map<String, Object>>(
+							0);
+					String respStr = EntityUtils
+							.toString(dresponse.getEntity());
+					if (respStr.startsWith("[")) {
+						JSONArray arr = new JSONArray(respStr);
+						for (int i = 0; i < arr.length(); i++) {
+							l.add(toMap(arr.getJSONObject(i)));
+						}
+					} else if (respStr.startsWith("{")) {
+						JSONObject json = new JSONObject(respStr);
+						Map<String, Object> mapa = toMap(json);
+						l.add(mapa);
+					}
+					callback.onFinish(l);
+				}
+			} catch (Exception e) {
+				System.out.println("Error: " + e);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public Map<String, Object> toMap(JSONObject json) {
 		Map<String, Object> mapa = new HashMap<String, Object>();
 		Iterator<String> iter = json.keys();
-	    while (iter.hasNext()) {
-	        String key = iter.next();
-	        try {
-	            mapa.put(key, json.get(key));
-	        } catch (JSONException e) {
-	        }
-	    }
-	    return mapa;
+		while (iter.hasNext()) {
+			String key = iter.next();
+			try {
+				mapa.put(key, json.get(key));
+			} catch (JSONException e) {
+			}
+		}
+		return mapa;
 	}
-	
+
 }
--- src/com/framework/simple/interfaces/DataSource.java
+++ src/com/framework/simple/interfaces/DataSource.java
@@ -1,6 +1,16 @@
 package com.framework.simple.interfaces;
 
+import java.util.Map;
 
 public interface DataSource {
-	public void getData(String apiSection, Callback callback);
+	public void getData(String apiSection, Map<String, String> params,
+			Callback callback);
+
+	public void postData(String apiSection, Map<String, String> params,
+			Callback callback);
+
+	void putData(String apiSection, Map<String, String> params,
+			Callback callback);
+
+	void deleteData(String apiSection, String id, Callback callback);
 }
